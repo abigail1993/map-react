f932973d9fa3259fdd0843918a1645d9efc52012
abigail1993 <alex.abi.roja@gmail.com>
2018-06-10 20:43:22 -0600

    solo de respaldo

--- maps-react/src/Map.js
+++ maps-react/src/Map.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 //import { compose, withProps, createEventHandlerWithConfig } from "recompose";
-import { /*withScriptjs, withGoogle,*/ GoogleMap, Marker, withGoogleMap } from "react-google-maps";
-//import data from './data'
+import { /*withScriptjs, withGoogle,*/  GoogleMap, Marker, withGoogleMap } from "react-google-maps";
 import data from "./datos";
 
 
@@ -16,20 +15,29 @@ class Map extends Component {
 
  //console.log(this.state.data.data[1].Coordinates) //  CON ESTO SE ENTRA A LAS COORDENADAS
 
+
+ const markers= this.state.data.data;
+        //console.log (markers);esto muestra en consola el arreglo del json
+
+     markers.forEach(function(element) {
+         let datos= element;
+          console.log(datos);//esto muestra por separado cada arreglo
+          });
     }   
 
-    render(){      
+    render(){   
     
 
        //  tengo que crear la funcion que recorra lo data y que devuelva las coordenadas
        // para que esas se agregen a un marker y las pinte en el mapa pero no se en donde
-        
         return(
+            
             <GoogleMap
                defaultZoom= {this.props.zoom}
                defaultCenter={this.props.center}>
-               <Marker position={{lat: 19.4203024, lng:  -99.1631142}}/*aqui deberiacambiar las cordenadas para que se pinten en el map*/ /> 
 
+               <Marker position={{lat: 19.4203024, lng:  -99.1631142}} /> 
+               <Marker position={{lat: 39.390897, lng: -99.066067}} /> 
             </GoogleMap>
             
         )
